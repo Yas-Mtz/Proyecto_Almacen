6332afa688b0058ee2dd6cae80e6110db05b067d
Daniela-Robles <daniela.robles@estudiante.uacm.edu.mx>
2025-02-21 20:33:37 -0600

    Reorganizando el proyecto en una sola carpeta

--- Sistema/Login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views  # Asegúrate de que la importación es correcta
-
-urlpatterns = [
-    path('', views.login, name='login'),
-    # Cambio logout a logout_view
-    path('logout/', views.logout, name='logout'),
-
-]
--- Sistema/Reportes/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.reportes, name='reportes'),
-    path('generar-reporte/', views.generar_reporte, name='generar_reporte'),
-]
