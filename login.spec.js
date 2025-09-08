614023722503d20bb431894191d671a2d0e434a0
Daniela-Robles <daniela.robles@estudiante.uacm.edu.mx>
2025-09-07 19:21:25 -0600

    modificacion de pruebas

--- login.spec.js
+++ login.spec.js
@@ -1,23 +1,54 @@
-import { test, expect, chromium } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
-test("login visible", async () => {
-  // Abrir navegador visible manualmente
-  const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
-  const page = await context.newPage();
-
-  // Ir a login
+test("test", async ({ page }) => {
   await page.goto("http://localhost:8000/login/");
-
-  // Llenar formulario
+  await page.getByRole("textbox", { name: "Usuario" }).click();
+  await page.getByRole("textbox", { name: "Usuario" }).press("CapsLock");
+  await page.getByRole("textbox", { name: "Usuario" }).fill("G");
+  await page.getByRole("textbox", { name: "Usuario" }).press("CapsLock");
+  await page.getByRole("textbox", { name: "Usuario" }).fill("Gerente1010");
+  await page.getByRole("textbox", { name: "Contraseña" }).click();
+  await page.getByRole("textbox", { name: "Contraseña" }).fill("cuautepec1010");
+  await page.getByRole("button", { name: "Ingresar" }).click();
+  await page.getByRole("button", { name: "Aceptar" }).click();
+  await page.getByRole("textbox", { name: "Usuario" }).click();
+  await page.getByRole("textbox", { name: "Usuario" }).press("CapsLock");
+  await page.getByRole("textbox", { name: "Usuario" }).fill("A");
+  await page.getByRole("textbox", { name: "Usuario" }).press("CapsLock");
   await page.getByRole("textbox", { name: "Usuario" }).fill("Ayudante0010");
+  await page
+    .locator("form div")
+    .filter({ hasText: "Contraseña" })
+    .locator("div")
+    .click();
+  await page
+    .locator("form div")
+    .filter({ hasText: "Contraseña" })
+    .locator("div")
+    .click();
   await page.getByRole("textbox", { name: "Contraseña" }).fill("cuautepec0010");
   await page.getByRole("button", { name: "Ingresar" }).click();
-
-  // Perfil y cerrar sesión
+  await page
+    .locator("div")
+    .filter({ hasText: "📦 GESTIÓN DE PRODUCTOS" })
+    .getByRole("link")
+    .click();
+  await page
+    .getByRole("combobox", { name: "Buscar productos por ID o" })
+    .click();
+  await page
+    .getByRole("combobox", { name: "Buscar productos por ID o" })
+    .fill("1");
+  await page.getByRole("button", { name: "Buscar producto" }).click();
+  await page.getByRole("textbox", { name: "Cantidad" }).click();
+  await page.getByRole("textbox", { name: "Cantidad" }).fill("0");
+  await page
+    .getByRole("button", { name: "Ajustar cantidad manualmente" })
+    .click();
+  await page.getByRole("button", { name: "Cancelar" }).click();
+  await page.getByRole("button", { name: " Actualizar Producto" }).click();
+  await page.getByRole("button", { name: "OK" }).click();
+  await page.getByRole("link", { name: "Inicio" }).click();
   await page.locator("#userProfile").getByText("Ayudante0010").click();
   await page.getByRole("link", { name: "Cerrar sesión" }).click();
-
-  // Cerrar navegador
-  await browser.close();
 });
---
+++ test-results/login-test/error-context.md
@@ -0,0 +1,36 @@
+# Page snapshot
+
+```yaml
+- generic [active] [ref=e1]:
+  - banner [ref=e2]:
+    - img "Logo UACM" [ref=e4]
+    - heading "SISTEMA DE INVENTARIO" [level=1] [ref=e5]
+    - generic [ref=e6] [cursor=pointer]:
+      - generic [ref=e7] [cursor=pointer]: 👤
+      - generic [ref=e8] [cursor=pointer]: Ayudante0010
+  - main [ref=e9]:
+    - paragraph [ref=e11]:
+      - text: Bienvenido
+      - generic [ref=e12]: Ayudante0010
+    - generic [ref=e13]:
+      - generic [ref=e14]:
+        - generic [ref=e15]: 📦
+        - heading "GESTIÓN DE PRODUCTOS" [level=2] [ref=e16]
+        - paragraph [ref=e17]: Administra los productos del almacén con registro, actualización y control de disponibilidad.
+        - link "→" [ref=e18] [cursor=pointer]:
+          - /url: /GestiondeProductos/
+      - generic [ref=e19]:
+        - generic [ref=e20]: ⏰
+        - heading "REPORTES E INVENTARIO" [level=2] [ref=e21]
+        - paragraph [ref=e22]: Genera reportes de solicitudes e inventarios por fecha para un mayor control del almacén.
+        - link "→" [ref=e23] [cursor=pointer]:
+          - /url: /Reportes/
+      - generic [ref=e24]:
+        - generic [ref=e25]: 🛒
+        - heading "SOLICITUD DE ARTÍCULOS" [level=2] [ref=e26]
+        - paragraph [ref=e27]: Elabora las solicitudes de productos.
+        - link "→" [ref=e28] [cursor=pointer]:
+          - /url: /Solicitudes/
+  - contentinfo [ref=e29]:
+    - paragraph [ref=e30]: "\"NADA HUMANO ME ES AJENO\""
+```
