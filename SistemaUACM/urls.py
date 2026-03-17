0e84d3f6d9f71596e672217e8b5e354ebc442595
Daniela-Robles <daniela.robles@estudiante.uacm.edu.mx>
2026-03-17 11:24:56 -0600

    Renombra views.py a controllers.py en Reportes y SistemaUACM para consistencia MVC

--- SistemaUACM/urls.py
+++ SistemaUACM/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from . import controllers as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
