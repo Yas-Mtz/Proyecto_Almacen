3b638feb314fc515fb9f4aab8467607d5bc631e0
Daniela-Robles <daniela.robles@estudiante.uacm.edu.mx>
2026-03-17 20:25:36 -0600

    Limpieza post-migración: elimina templates jQuery y JS obsoletos
    - Elimina frontend_uacm/build/*.html (5 templates HTML/jQuery)
    - Elimina frontend_uacm/build/static/js/ (scripts jQuery)
    - Remueve build/ de TEMPLATES DIRS en settings.py
    - Se conserva build/static/css/ y build/static/media/ (referenciados por React)

--- SistemaUACM/settings.py
+++ SistemaUACM/settings.py
@@ -64,7 +64,6 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'frontend_uacm/build-react'),
-            os.path.join(BASE_DIR, 'frontend_uacm/build'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
